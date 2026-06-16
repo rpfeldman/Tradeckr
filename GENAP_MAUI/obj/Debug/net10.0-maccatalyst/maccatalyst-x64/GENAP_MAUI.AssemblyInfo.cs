c501101aec1234f4ec89a3bc3238c19d7241e003
rpfeldman <ramirofeldman0@gmail.com>
2026-06-16 12:33:55 -0300

    Fixed TransactionCategoriesPage: the 'save button' was hidden in the grid

--- GENAP_MAUI/obj/Debug/net10.0-maccatalyst/maccatalyst-x64/GENAP_MAUI.AssemblyInfo.cs
+++ GENAP_MAUI/obj/Debug/net10.0-maccatalyst/maccatalyst-x64/GENAP_MAUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GENAP_MAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9bdb9338ff9a45a283b0ffe23c08bd2807b9b978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9c5f6f1343e3645ba22e5dcca54065c94140803b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GENAP_MAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GENAP_MAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
