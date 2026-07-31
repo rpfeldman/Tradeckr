840d108ec211dc4724a96c523b00a1cf14284d4e
rpfeldman <ramirofeldman0@gmail.com>
2026-07-31 17:46:42 -0300

    Add ControlBehaviours folder and MoneyEntryBehaviour behaviour for entries

---
+++ GENAP_MAUI/ControlBehaviours/MoneyEntryBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GENAP_MAUI.ControlBehaviours
+{
+    public sealed class MoneyEntryBehaviour : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.TextChanged += Entry_TextChanged;
+        }
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.TextChanged -= Entry_TextChanged;
+        }
+
+        private void Entry_TextChanged(object? sender, TextChangedEventArgs e)
+        {
+            var entry = (Entry)sender!;
+
+            if(int.Parse(e.NewTextValue) == 67)
+            {
+                entry.Text = "666";
+            }
+        }
+    }
+}
