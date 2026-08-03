268673b31cd5b75a376d713cdfe9eba75fc57df7
rpfeldman <ramirofeldman0@gmail.com>
2026-08-03 12:02:17 -0300

    add the auto-format functionality to MoneyEntryBehaviour

--- GENAP_MAUI/ControlBehaviours/MoneyEntryBehaviour.cs
+++ GENAP_MAUI/ControlBehaviours/MoneyEntryBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace GENAP_MAUI.ControlBehaviours
@@ -22,11 +23,30 @@ namespace GENAP_MAUI.ControlBehaviours
         private void Entry_TextChanged(object? sender, TextChangedEventArgs e)
         {
             var entry = (Entry)sender!;
+            var culture = CultureInfo.CurrentCulture;
 
-            if(int.Parse(e.NewTextValue) == 67)
+            if (string.IsNullOrWhiteSpace(e.NewTextValue)) { return; }
+            if (e.NewTextValue[^1] == culture.NumberFormat.CurrencyDecimalSeparator[0]) { return; }
+
+            if(!decimal.TryParse(entry.Text, out decimal value))
             {
-                entry.Text = "666";
+                entry.Text = string.Empty;
             }
+
+            if(value % 1 != 0)
+            {
+                entry.Dispatcher.Dispatch(() =>
+                {
+                    entry.Text = value.ToString("N2", culture);
+                });
+
+                return;
+            }
+
+            entry.Dispatcher.Dispatch(() =>
+            {
+                entry.Text = value.ToString("N0", culture);
+            });
         }
     }
 }
