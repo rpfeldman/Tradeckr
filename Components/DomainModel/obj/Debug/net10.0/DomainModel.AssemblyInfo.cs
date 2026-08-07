4ae79f0346971b56c7e2219f87175a4e95d1d97f
rpfeldman <ramirofeldman0@gmail.com>
2026-08-07 19:05:18 -0300

    update the splash svg

--- Components/DomainModel/obj/Debug/net10.0/DomainModel.AssemblyInfo.cs
+++ Components/DomainModel/obj/Debug/net10.0/DomainModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DomainModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aab8331596b4b790fbc9f12446a956c32f3ef57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c598797ed4e8e509ad345168c90416f5d2ca5966")]
 [assembly: System.Reflection.AssemblyProductAttribute("DomainModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DomainModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
