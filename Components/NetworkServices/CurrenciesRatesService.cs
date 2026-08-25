be601c7338fe467459b41417ab4656d67cac92a3
rpfeldman <ramirofeldman0@gmail.com>
2026-08-25 13:39:21 -0300

    Add GetRates method in CurrenciesRatesService

---
+++ Components/NetworkServices/CurrenciesRatesService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using DomainModel;
+using System.Text.Json.Serialization;
+using System.Reflection.Metadata;
+using System.Net.Mime;
+
+namespace NetworkServices
+{
+    public sealed class CurrenciesRatesService(string rootCurrencyIsoCode)
+    {
+        private string _RootCurrencyIsoCode = rootCurrencyIsoCode;
+        private HttpClient _httpClient = new();
+
+        public async Task<OperationResult<Dictionary<string, decimal>>> GetRatesAsync(DateOnly date)
+        {
+            if (!NetworkMethods.CheckInternetConnection())
+            {
+                return OperationResult<Dictionary<string, decimal>>.FaultedOperation(new InnerErrorDto()); // TO-DO
+            }
+
+            var formattedDate = date.ToString("yyyy-MM-dd");
+
+            var request = await _httpClient.GetAsync($"https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{formattedDate}/v1/currencies/{_RootCurrencyIsoCode}.json");
+
+            if (!request.IsSuccessStatusCode)
+            {
+                return OperationResult<Dictionary<string, decimal>>.FaultedOperation(new InnerErrorDto()); // TO-DO
+            }
+
+            var content = await request.Content.ReadAsStringAsync();
+
+            content = content.Substring(0, content.IndexOf(_RootCurrencyIsoCode)) + "Rates" + content.Substring(content.IndexOf(_RootCurrencyIsoCode) + _RootCurrencyIsoCode.Length); // we replace the isocode of the JSON text to 'Rates' to match the property
+
+            var result = JsonSerializer.Deserialize<CurrencyJsonDto>(content)?.Rates;
+            return OperationResult<Dictionary<string, decimal>>.SuccessfulOperation(result!);
+        }
+    }
+
+    sealed class CurrencyJsonDto
+    {
+        [JsonPropertyName("date")]
+        public DateOnly Date { get; set; }
+        public Dictionary<string, decimal> Rates { get; set; } = [];
+    }
+}
--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -11,6 +11,7 @@ using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using System.Globalization;
 using System.Security;
 using System.Runtime.CompilerServices;
+using NetworkServices;
 
 namespace ConsoleTest 
 {
@@ -24,79 +25,23 @@ namespace ConsoleTest
             DataProjectionService dps = new(repo);
             DataManagementService dms = new(repo);
             DataRegistrationService drs = new(repo);
+
+            string isocode = "sxo";
+
+            CurrenciesRatesService crs = new(isocode);
+            
             var today = DateOnly.FromDateTime(DateTime.Today);
 
-            CurrencyDto peso = new() { ConversionRate = 1498.2540m, CurrencyDisplayName = "Peso argentino", CurrencyId = 0, IsoCode = "ARS" };
-            CurrencyDto dolar = new() { ConversionRate = 1, CurrencyDisplayName = "Dolar estadounidense", CurrencyId = 1, IsoCode = "USD" };
-            CurrencyDto euro = new() { ConversionRate = 0.8662m, CurrencyDisplayName = "Euro", CurrencyId = 2, IsoCode = "EUR" };
-            CurrencyDto yen = new() { ConversionRate = 159.1480m, CurrencyDisplayName = "Yen japones", CurrencyId = 3, IsoCode = "JPY" };
-            CurrencyDto peso_uruguayo = new() { ConversionRate = 40.2576m, CurrencyDisplayName = "Peso urugayo", CurrencyId = 4, IsoCode = "UYU" };
-            CurrencyDto rand = new() { ConversionRate = 16.1912m, CurrencyDisplayName = "Rand sudafricano", CurrencyId = 5, IsoCode = "ZAR" };
-            CurrencyDto real = new() { ConversionRate = 5.1076m, CurrencyDisplayName = "Real brasileño", CurrencyId = 6, IsoCode = "BRL" };
+            var getratesop = await crs.GetRatesAsync(today);
 
-            CurrencyDto[] Currencies = new CurrencyDto[] { peso, dolar, euro, yen, peso_uruguayo, rand, real };
-
-
-            foreach (var item in Currencies)
+            if (getratesop.Success)
             {
-                Console.WriteLine($"Aqui estan tus movimientos representados en {item.CurrencyDisplayName} - {item.IsoCode}$ \n------\n");
-
-                var getoperation = await dps.ProjectTransactions<TransactionDto>(t => new() { Value = CurrencyConverterService.TfuToCurrency(t.Value, item), Category = t.Category, Date = t.Date, Depletion = t.Depletion, Fixed = t.Fixed, Id = t.Id });
-
-                if (getoperation.Success)
+                foreach (var item in getratesop.Result!)
                 {
-                    foreach (var transaction in getoperation.Result!)
-                    {
-                        Console.WriteLine("se "+(transaction.Depletion ? "gasto " : "gano ")+$"{transaction.Value:N8} {item.IsoCode}$ en {transaction.Category}");
-                    }
-                }
-
-                Console.WriteLine("\n------\n");
-            }
-
-            /*
-            while (true)
-            {
-                Console.WriteLine("Registra un movimiento flaco, dale");
-                Console.Write("Escribi la categoria pibe dale: ");
-                string categoria = Console.ReadLine() ?? "Uncategorized";
-                Console.Write("Escribi el valor en pesos: ");
-                _ = decimal.TryParse(Console.ReadLine(), out decimal value);
-                Console.Write("Decime si es un gasto o un ingreso pibe dale (1/0): ");
-                int.TryParse(Console.ReadLine(), out int Ndepletion);
-                bool depletion = Ndepletion == 1;
-
-                if (depletion)
-                {
-                    var op = await drs.RegistExpenseAsync(CurrencyConverterService.CurrencyToTfu(value, peso), today, categoria);
-
-                    if (op.Success)
-                    {
-                        Console.WriteLine("\n---\nGasto registrado con exito pibardo!\n---\n");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Algo fallo xd"+" "+op.InnerError?.ErrorMessage);
-                        break;
-                    }
-
-                    continue;
-                }
-
-                var op2 = await drs.RegistIncomeAsync(CurrencyConverterService.CurrencyToTfu(value, peso), today, categoria);
-
-                if (op2.Success)
-                {
-                    Console.WriteLine("\n---\nIngreso registrado con exito pibardo!\n---\n");
-                }
-                else
-                {
-                    Console.WriteLine("Algo fallo xd" + " " + op2.InnerError?.ErrorMessage);
-                    break;
+                    Console.WriteLine($"{isocode} vs {item.Key}: 1 : {item.Value:N3}");
                 }
             }
-            */
-
+            else { Console.WriteLine(getratesop.InnerError!.ErrorMessage); }
         }
     }
 }
