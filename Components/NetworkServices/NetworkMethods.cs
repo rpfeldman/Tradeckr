5e6e97707697b43a071143138e9b8f508ba5ada3
rpfeldman <ramirofeldman0@gmail.com>
2026-08-25 12:30:05 -0300

    Add NetworkServices layer

---
+++ Components/NetworkServices/NetworkMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace NetworkServices
+{
+    public static class NetworkMethods
+    {
+        public static bool CheckInternetConnection()
+        {
+            if (!NetworkInterface.GetIsNetworkAvailable())
+            {
+                return false;
+            }
+
+            try
+            {
+                using (Ping ping = new Ping())
+                {
+                    PingReply reply = ping.Send("8.8.8.8", 2000);
+                    return reply.Status == IPStatus.Success;
+                }
+            }
+            catch (PingException)
+            {
+                return false;
+            }
+        }
+    }
+}
