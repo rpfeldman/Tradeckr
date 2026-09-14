4216e69775e5c2b6036554a47be655a0154a91ec
rpfeldman <ramirofeldman0@gmail.com>
2026-09-14 12:45:12 -0300

    Add temporal debug logging in LoadingPage

--- GENAP_MAUI/Pages/IntermediaryPages/LoadingPage.xaml.cs
+++ GENAP_MAUI/Pages/IntermediaryPages/LoadingPage.xaml.cs
@@ -1,4 +1,5 @@
 using CommunityToolkit.Maui.Views;
+using Serilog;
 
 namespace GENAP_MAUI.Pages.IntermediaryPages;
 
@@ -7,24 +8,34 @@ public partial class LoadingPage : ContentPage
 	public LoadingPage()
 	{
 		InitializeComponent();
+
+        Log.Debug("Loading page initialized");
 	}
 
     private async void MediaElement_MediaEnded(object sender, EventArgs e)
     {
+            Log.Debug("Media ended");
+
+            Log.Debug("Loading page waiting AppLoadingResetEvent");
         GlobalResources.AppLoadingResetEvent.Wait();
+            Log.Debug("Loading page advanced AppLoadingResetEvent");
 
         await Dispatcher.DispatchAsync(async () =>
         {
             if (GlobalResources.IsNewUser)
             {
                 GlobalResources.AppLoadingResetEvent.Dispose();
+                    Log.Debug("AppLoadingResetEvent disposed");
 
+                    Log.Debug("Moving to OnBoarding page");
                 await Shell.Current.GoToAsync($"//{Routes.Onboarding}");
                 return;
             }
 
             GlobalResources.AppLoadingResetEvent.Dispose();
+                Log.Debug("AppLoadingResetEvent disposed");
 
+                Log.Debug("Moving to MainDashboard page");
             await Shell.Current.GoToAsync($"//{Routes.Dashboard}");
         });
     }
@@ -37,11 +48,13 @@ public partial class LoadingPage : ContentPage
         mediaElement.Dispatcher.Dispatch(() =>
         {
             mediaElement.Play();
+                Log.Debug("Media started");
         });
     }
 
     private void SplashAnimation_MediaFailed(object sender, CommunityToolkit.Maui.Core.MediaFailedEventArgs e)
     {
         System.Diagnostics.Debug.WriteLine($"Media failed: {e.ErrorMessage}");
+            Log.Error($"Media failed: {e.ErrorMessage}");
     }
 }
